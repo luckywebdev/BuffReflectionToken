649ba6a5673d9bd8349489698f9e372cf9402278
luckywebdev <webdevelop827@gmail.com>
2021-08-12 04:09:31 +0700

    Add token exchange contract

---
+++ contracts/TokenExchange.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.4;
+
+import "./utils/IOfficialBuffDoge.sol";
+import "./utils/Ownable.sol";
+import "./utils/SafeMath.sol";
+
+contract TokenExchange is Ownable {
+    constructor () {
+
+    }
+
+}
